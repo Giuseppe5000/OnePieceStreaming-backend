066c4abecf9ce9cc4f90787dff69dd19b8e65457
Giuseppe <giuseppetutino1@gmail.com>
2022-09-24 11:52:54 +0200

    Vercel dotenv fix

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,12 +1,12 @@
-const mysql = require('mysql');
-let dotenv = require('dotenv').config();
+const mysql = require("mysql");
+//let dotenv = require('dotenv').config()
 
 // DB connect
 const connection = mysql.createPool({
-    host: dotenv.parsed.HOST,
-    user: dotenv.parsed.USER,
-    password: dotenv.parsed.PASS,
-    database: dotenv.parsed.DB
+    host: process.env.HOST, //dotenv.parsed.HOST,
+    user: process.env.USER, //dotenv.parsed.USER,
+    password: process.env.PASS, //dotenv.parsed.PASS,
+    database: process.env.DB, //dotenv.parsed.DB
 })
 
 module.exports = connection;
